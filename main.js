691830677b3b55d4aafa28fa54eb84801846546a
unknown <d.kolodinskaya02@gmail.com>
2024-07-15 16:50:07 +0300

    add modal

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 import './style.css'
 import './demo_block.scss'
+import './modal.scss'
 
-import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const modalTrigger = document.getElementsByClassName("trigger")[0];
 
-// setupCounter(document.querySelector('#counter'))
+const modalBackground = document.getElementsByClassName("modalBackground")[0];
+const modalClose = document.getElementsByClassName("modalClose")[0];
+
+
+
+modalTrigger.addEventListener("click", function () {
+ 
+    modalBackground.style.display = "block";
+
+});
+modalClose.addEventListener("click", function () {
+    modalBackground.style.display = "none";
+});
+
+
+modalBackground.addEventListener("click", function (event) {
+    if (event.target === modalBackground) {
+        modalBackground.style.display = "none";
+    }
+});
+
+
